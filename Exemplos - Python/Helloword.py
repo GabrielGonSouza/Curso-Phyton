cf6876d806ed2833e0f218414a8e0af539dfbfde
GabrielGonSouza <gabrielsouza07222@gmail.com>
2025-11-19 11:50:03 -0300

    Add files via upload

---
+++ Exemplos - Python/Helloword.py
@@ -0,0 +1,12 @@
+print("Olá Mundo! ")
+#Declarando as variaveis do python 
+Nome = input("Digite seu nome:")
+Idade= input("Digite sua idade:")
+
+#exibir os dados de uma variavel 
+print("Nome: ", Nome)
+print("Idade: ", Idade)
+
+print(f"Nome do usuário: {Nome}")
+print(f"Idade do usuário: {Idade}")
+print(f"Olá {Nome}, sua Idade é {Idade}")
